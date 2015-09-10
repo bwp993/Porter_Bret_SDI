bbd5f25b6caa9339b8f3a43007ac5bd5f60e76c0
Bret Porter <bwporter@fullsail.edu>
2015-09-09 19:46:55 -0700

    Calculated the users gas mileage
    Calculated the users gas mileage using the number of miles they drove
    and the amount of gas they used.

--- Porter_Bret_Expressions_Assignment/js/expression.js
+++ Porter_Bret_Expressions_Assignment/js/expression.js
@@ -6,4 +6,19 @@ Title
 Date
  */
 
+//alert("testing");
+
+ //Ask for the users name
+ var name= prompt("Let's calculate your gas mileage! \n I would like to know your name, please enter it below.");
+ alert("Hello "+name+ ", Let's get started!");
+
+ //Ask for amount of gas consumed
+ var gasUsed= prompt("How many gallons of gas did you use on your last trip?");
+
+ //Ask for amount of miles travelled
+ var milesTravelled= prompt("How many miles did you travel on that trip?");
+
+ //Calculate the user's miles per gallon
+ var milesPerGallon= milesTravelled/gasUsed;
+ alert("On your last trip your gas mileage was "+ milesPerGallon + " miles per gallon!");
 
