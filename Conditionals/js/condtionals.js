d33b4ec95d26ab1cfab573410e364aedd45114ae
Bret Porter <bwporter@fullsail.edu>
2015-09-16 11:35:35 -0700

    First screencast
    completed the first screencast and followed along

--- Conditionals/js/condtionals.js
+++ Conditionals/js/condtionals.js
@@ -5,3 +5,13 @@ SDI Section #1
 Conditionals
 09-16-2015
  */
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
+else(console.log("What comes after"))
+
