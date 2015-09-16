28529861b58d6b886c9bec3f0bedd912be3b770d
Bret Porter <bwporter@fullsail.edu>
2015-09-16 12:23:37 -0700

    screencast && operator
    practiced with the && operator

--- Conditionals/js/condtionals.js
+++ Conditionals/js/condtionals.js
@@ -23,3 +23,16 @@ if(kidHeight > minHeight){
     console.log("Sorry kid, you've got some growing to do first.")
 }
 
+
+//Conditional Logic - Logical Operators
+
+ var budget = 300
+ var iPhonePrice = 199.99
+ var paycheck = 200
+
+//if the price of the phone is less than our budget AND our paycheck over 300
+ if(iPhonePrice < budget && paycheck > 300){
+     console.log("We can buy the phone!");
+ }else{
+     console.log("No phone for you!");
+ }
