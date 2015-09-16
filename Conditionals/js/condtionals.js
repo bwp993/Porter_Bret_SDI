347e617c3952dab25da776b89fe613698ad225b0
Bret Porter <bwporter@fullsail.edu>
2015-09-16 11:48:46 -0700

    screencast #3
    Followed along with screencast #3

--- Conditionals/js/condtionals.js
+++ Conditionals/js/condtionals.js
@@ -6,10 +6,13 @@ Conditionals
 09-16-2015
  */
 
-var oldEnough = false;
+var kidHeight = 50;
+var minHeight = 48
+var sneakerLift = 2
 
 //if the child is old enough, print to the console "you can ride!"
-if(oldEnough){
+//if the kid is over 48 inches in height
+if(kidHeight + sneakerLift > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
