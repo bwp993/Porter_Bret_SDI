9a0482668101a1dbf7b039cc46ed81e358be49d8
Bret Porter <bwporter@fullsail.edu>
2015-09-21 07:17:18 -0700

    Began calculator
    Started calculator and asked for user’s name

---
+++ Porter_Bret_Functions_Assignment/js/functionsAssignment.js
@@ -0,0 +1,18 @@
+
+ /*
+Bret Porter
+SDI Section #1
+Functions Assignment
+09-20-2015
+ */
+
+/*Create a calculator to calculate the amount of space for luggage
+ in a vehicle (Area of trunk) and the area of the luggage.
+*/
+
+//Prompt user for their name and make sure one is entered.
+var name = prompt("Hello friend, want to know if you have enough luggage space for your trip? \nLet's" +
+    " start with getting to know you. \nWhat's your name?");
+ if(name===""){
+  prompt("Don't leave this blank, please enter your name.);
+ }
