5d0a1d0b136f0abc8f96e9e83bd7e5131fe56039
Bret Porter <bwporter@fullsail.edu>
2015-09-20 18:59:52 -0700

    do while loops
    Followed along with do while loops video

--- Loops/js/loops.js
+++ Loops/js/loops.js
@@ -14,3 +14,13 @@ while (b > 0){ //checks the condition
  console.log(b + "kegs on the wall.");
  b--; //increments or decrements the index.
 }
+
+//Do While loop. Code will be ran the first time even if the 'while' is false.
+
+var c = 10;
+
+do{
+ console.log(c + " kegs on the wall.");
+ c--;
+}
+while(c > 0);
