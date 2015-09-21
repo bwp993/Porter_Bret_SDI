823b16fcb10885960d98da68bdecfceb80fdb671
Bret Porter <bwporter@fullsail.edu>
2015-09-20 19:38:28 -0700

    set up gototraining week 4 files
    set up gototraining week4 files

---
+++ GoToTrainingWeek4/js/GoToTraining4.js
@@ -0,0 +1,8 @@
+
+ /*
+Bret Porter
+SDI Section #1
+GoToTrainingWeek4
+9-20-2015
+ */
+ 
--- Loops/js/loops.js
+++ Loops/js/loops.js
@@ -24,3 +24,25 @@ do{
  c--;
 }
 while(c > 0);
+
+//For Loop
+
+for (var i = 10; i > 0; i--){
+ console.log(i + "kegs on the wall.")
+}
+
+/*example of 'break' -- if(i==100){
+ break;
+ } This means that once the loop gets to i==100 it will move out of the bottom of the loop
+ */
+
+/*example of 'continue'
+continue moves back to the top of the loop once the 'if' is met
+for(var i = 1; i < 5000; i++){
+ do stuff
+ do stuff
+ if(i % 5 == 0){
+ continue; //done with thei iteration/stops at this run of the loop and move
+ back to the top.
+}
+*/
